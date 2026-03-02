2ed8fc697d52d5e5d5558791e0c596b3a753fdf0
danielmilchev03 <daniel.milchev03@gmail.com>
2026-03-02 22:49:16 +0200

    Added Google Analytics support

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,26 @@ import '../styles/globals.css';
 import { SessionProvider } from 'next-auth/react';
 import Head from 'next/head';
 import { ThemeProvider } from '../contexts/ThemeContext';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      if (window.gtag) {
+        window.gtag('config', process.env.NEXT_PUBLIC_GA_ID, {
+          page_path: url,
+        });
+      }
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <SessionProvider session={session}>
       <ThemeProvider>
